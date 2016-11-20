46295bc7bcb31ceb44e421dcadd66adeb8f30017
Bikash Narayan Panda <oksbwn@gmail.com>
2016-11-20 22:54:45 +0530

    Updated Code

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,13 +1,11 @@
 #include <SoftwareSerial.h>
-#define WIFI_SSID        "oksbwn-world"
-#define WIFI_PASS        "omisoksbwn" // My luggage has the same combination!
-#define SERVER_IP     "192.168.0.1"
-#define TIMEOUT     5000 // mS
+#define WIFI_SSID        "#####" //WIFI SSID
+#define WIFI_PASS        "********" // Wifi Password
+#define SERVER_IP     "192.168.0.1" // IP of the target server.
+#define TIMEOUT     5000 //Timeout for the serial communication
 #define CONTINUE    false
 #define HALT        true
-#define NO_OF_PARAMETERS 1
-
-// #define ECHO_COMMANDS // Un-comment to echo AT+ commands to serial monitor
+#define NO_OF_PARAMETERS 1 //No of parameters sending to the server.
 
 SoftwareSerial esp8266SerialPort(10, 11); // RX, TX
 
@@ -17,17 +15,17 @@ void exception(String msg){ //Exception occured. Print msg and stops.
   while(true){
    readResponseData("");
    delay(60000);
-  };
+  }
 }
 boolean readResponseData(String keyword){ //Receive data from the serial port. Returns true if keyword matches.
   String response;
   long deadline = millis() + TIMEOUT;
   while(millis() < deadline){
     if (esp8266SerialPort.available()){
-      char ch = esp8266SerialPort.read();
+      char ch = esp8266SerialPort.read(); // Read one character from serial port and append to a string.
       response+=ch;
       if(keyword!=""){
-        if(response.indexOf(keyword)>0){
+        if(response.indexOf(keyword)>0){ //Searched keyword found.
           Serial.println(response);
           return true;
         }
@@ -41,11 +39,6 @@ boolean readResponseData(String keyword){ //Receive data from the serial port. R
 boolean sendCommand(String command, String acknowledgement, boolean stopOnError)
 {
   esp8266SerialPort.println(command);
-  #ifdef ECHO_COMMANDS
-  Serial.print("--"); 
-  Serial.println(command);
-  #endif
-
   if (!readResponseData(acknowledgement)) 
     if (stopOnError)
       exception(command+" Failed to execute.");
@@ -90,18 +83,18 @@ void setup()
 
 void loop() 
 {
-  String cmd = "AT+CIPSTART=0,\"TCP\",\""; cmd += SERVER_IP; cmd += "\",80";
+  String cmd = "AT+CIPSTART=0,\"TCP\",\""; cmd += SERVER_IP; cmd += "\",80"; //Start a TCP connection.  to server SERVER_IP on port 80
   if (!sendCommand(cmd, "OK", CONTINUE)) 
     return;
   delay(2000);
 
-  if (!sendCommand("AT+CIPSTATUS", "OK", CONTINUE))
+  if (!sendCommand("AT+CIPSTATUS", "OK", CONTINUE))// Check for TCP Connection status.
     return;
     
-  String  tag[NO_OF_PARAMETERS]={"NAME"};
-  String data[NO_OF_PARAMETERS]={"my Name"};
+  String  tag[NO_OF_PARAMETERS]={"NAME"}; //Tags for the parameters
+  String data[NO_OF_PARAMETERS]={"my Name"}; //Values for the parameters
   
-  cmd = "GET /ESP8266/index.php?";
+  cmd = "GET /ESP8266/index.php?";//Path to the target server file.
   for(int count=0;count<NO_OF_PARAMETERS;count++){
     if(count!=0)
       cmd+="&";
@@ -116,8 +109,9 @@ void loop()
     return;
   }
 
-  sendCommand(cmd, "OK", CONTINUE);  // GET
+  sendCommand(cmd, "OK", CONTINUE);// Send data to server.
   
-  readResponseData("");
+  readResponseData("");// Read response data received from server.
   exception("ONCE ONLY");
 }
+
